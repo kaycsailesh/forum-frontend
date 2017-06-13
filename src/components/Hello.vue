95ccdafb7e5ecdbb9c80d3e545c890f291ffe40a
Sailesh Kc <sailesh@Saileshs-MacBook-Pro.local>
2017-06-13 20:22:47 +1000

    Generating Structure

---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+     visible: false
+    }
+  }
+}
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="test">
+    <p>test</p>
+  </div>
+</template>
+<script>
+  export default {
+    name:'test',
+    data(){
+
+    }
+  }
+</script>
